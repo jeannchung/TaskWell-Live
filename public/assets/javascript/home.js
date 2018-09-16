9f1e5b076d283687d9eb74d91bd6ef30cbfea84a
jean.chung <jeannchung@gmail.com>
2018-09-15 22:23:02 -0700

    initial template of work

---
+++ public/assets/javascript/home.js
@@ -0,0 +1,9 @@
+// Home Java Script
+
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+  }
+  
+  function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+  }
---
+++ public/assets/javascript/index.js
@@ -0,0 +1,30 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyCG_jjezg8ta2ihiDEmWYdG7PdCkFlwk8o",
+    authDomain: "taskwell-f8bae.firebaseapp.com",
+    databaseURL: "https://taskwell-f8bae.firebaseio.com",
+    projectId: "taskwell-f8bae",
+    storageBucket: "taskwell-f8bae.appspot.com",
+    messagingSenderId: "257454357176"
+};
+firebase.initializeApp(config);
+// FirebaseUI config.
+var uiConfig = {
+    signInSuccessUrl: 'home.html',
+    signInOptions: [
+        // Leave the lines as is for the providers you want to offer your users.
+        firebase.auth.EmailAuthProvider.PROVIDER_ID],
+    // tosUrl and privacyPolicyUrl accept either url string or a callback
+    // function.
+    // Terms of service url/callback.
+    tosUrl: '<your-tos-url>',
+    // Privacy policy url/callback.
+    privacyPolicyUrl: function () {
+        window.location.assign('<your-privacy-policy-url>');
+    }
+};
+
+// Initialize the FirebaseUI Widget using Firebase.
+var ui = new firebaseui.auth.AuthUI(firebase.auth());
+// The start method will wait until the DOM is loaded.
+ui.start('#container', uiConfig);
