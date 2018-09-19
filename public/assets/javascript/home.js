294b99be8d42f024bef25385ba05c8ff70240c5f
jean.chung <jeannchung@gmail.com>
2018-09-18 19:22:58 -0700

    js update

--- public/assets/javascript/home.js
+++ public/assets/javascript/home.js
@@ -17,49 +17,18 @@ initApp = function () {
   firebase.auth().onAuthStateChanged(function (user) {
       if (user) {
           // User is signed in.
-          var displayName = user.displayName;
-          var email = user.email;
-          var emailVerified = user.emailVerified;
-          var photoURL = user.photoURL;
-          var uid = user.uid;
-          var phoneNumber = user.phoneNumber;
-          var providerData = user.providerData;
-          user.getIdToken().then(function (accessToken) {
-              document.getElementById('sign-in-status').textContent = 'Signed in';
-              document.getElementById('sign-in').textContent = 'Sign out';
-              document.getElementById('account-details').textContent = JSON.stringify({
-                  displayName: displayName,
-                  email: email,
-                  emailVerified: emailVerified,
-                  phoneNumber: phoneNumber,
-                  photoURL: photoURL,
-                  uid: uid,
-                  accessToken: accessToken,
-                  providerData: providerData
-              }, null, '  ');
-          });
-      } else {
-          // User is signed out.
-          document.getElementById('sign-in-status').textContent = 'Signed out';
-          document.getElementById('sign-in').textContent = 'Sign in';
-          document.getElementById('account-details').textContent = 'null';
-      }
+          console.log(user.uid)
+      } 
   }, function (error) {
-      console.log(error);
-  });
-};
+      console.log(error)
+  })
+}
 
 window.addEventListener('load', function () {
   initApp()
-});
+})
 
-function openForm() {
-    document.getElementById("myForm").style.display = "block";
-  }
-  
-  function closeForm() {
-    document.getElementById("myForm").style.display = "none";
-  }
+
 
   
 // Styles JS
