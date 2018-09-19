9150e3f7a9590855ac41f68a2c8fa64c906c2444
Garrett Fermo <garrettcfermo@gmail.com>
2018-09-18 19:07:44 -0700

    Home Page JS Creatation

--- public/assets/javascript/home.js
+++ public/assets/javascript/home.js
@@ -11,7 +11,7 @@ var config = {
 };
 firebase.initializeApp(config);
 
-//
+
 
 initApp = function () {
   firebase.auth().onAuthStateChanged(function (user) {
@@ -60,3 +60,64 @@ function openForm() {
   function closeForm() {
     document.getElementById("myForm").style.display = "none";
   }
+
+  
+// Styles JS
+document.addEventListener('DOMContentLoaded', function () {
+    var elems = document.querySelectorAll('.fixed-action-btn');
+    var instances = M.FloatingActionButton.init(elems, {
+        direction: 'left',
+        hoverEnabled: false
+    });
+});
+
+$(document).ready(function () {
+    $('.sidenav').sidenav();
+});
+
+$(document).ready(function () {
+    $('.collapsible').collapsible();
+});
+
+
+// Add Boards
+$('.addboard-btn').on('click', function () {
+    var boardName = $('.newBoardName').val().trim()
+
+    if (boardName !== "" && boardName.length <= 40) {
+        $('#myUL').append(`
+           <li class="collection-item ${boardName}"><a href="#!" class="collection-item Work"><span class="badge"><i class="small material-icons waves-effect delete-btn"
+              data-id="${boardName}">delete</i></span>
+          ${boardName}</a></li>
+          `)
+        $('.newBoardName').val('')
+    }
+
+})
+
+//Deleting Boards 
+$(document).on('click', '.delete-btn', function () {
+    var dataId = $(this).attr('data-id')
+    $('.collection-item.' + dataId).remove()
+
+})
+
+//SEARCH BOARDS
+function myFunction() {
+    // Declare variables
+    var input, filter, ul, li, a, i;
+    input = document.getElementById('myInput');
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("myUL");
+    li = ul.getElementsByTagName('li');
+
+    // Loop through all list items, and hide those who don't match the search query
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("a")[0];
+        if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
+}
