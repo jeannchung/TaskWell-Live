cc4b2dcb3c3f10e8a2ae642542e60244ced7e65e
Garrett Fermo <garrettcfermo@gmail.com>
2018-09-22 16:13:07 -0700

    Added the API Quote to Index Page

--- public/assets/javascript/index.js
+++ public/assets/javascript/index.js
@@ -36,7 +36,24 @@ var ui = new firebaseui.auth.AuthUI(firebase.auth());
 // The start method will wait until the DOM is loaded.
 ui.start('#container', uiConfig);
 
+// API Quote
+function randomQuote() {
+    $.ajax({
+        url: "https://api.forismatic.com/api/1.0/?",
+        dataType: "jsonp",
+        data: "method=getQuote&format=jsonp&lang=en&jsonp=?",
+        success: function (response) {
+            console.log(response.quoteText)
+            $('.quote-section').html(`
+        <p class="quote">"${response.quoteText}"</p>
+      `)
+        }
+    });
+}
 
+$(function () {
+    randomQuote();
+});
 
 
 
